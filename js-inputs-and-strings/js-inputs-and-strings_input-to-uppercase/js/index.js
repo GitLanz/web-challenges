9720ac581afd76183aa090ca488b8881f422da8d
Brigitte Lanz <brigitte.lanz.neuefische@gmail.com>
2025-02-17 15:58:42 +0100

    input to uppercase

--- js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,13 @@
 console.clear();
+// const input = document.querySelector(`[data-js="first-input"]`);
+// const button = document.querySelector(`[data-js="button-uppercase"]`);
+// button.addEventListener("click", function () {
+//   console.log("clicked");
+//   input.value = input.value.toUpperCase();
+// });
+
+const input = document.querySelector(`[data-js="first-input"]`);
+const button = document.querySelector(`[data-js="button-uppercase"]`);
+button.addEventListener("click", () => {
+  input.value = input.value.toUpperCase();
+});
