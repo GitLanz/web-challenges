80daf6a68b4e35543d4534bb63c0c9ed45b96854
Brigitte Lanz <brigitte.lanz.neuefische@gmail.com>
2025-02-18 12:40:03 +0100

    create alert messages

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,9 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+// --v-- write your code here --v--
+const successMessage = document.querySelector('[data-js="success"]');
+// --^-- write your code here --^--
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,9 +19,27 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  if (tosCheckbox.checked === false) {
+    showTosError();
+    return;
+  } else {
+    hideTosError;
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+  // --v-- write your code here --v--
+  successMessage.removeAttribute("hidden");
+  // --^-- write your code here --^--
 });
+
+// --v-- write your code here --v--
+tosCheckbox.addEventListener("click", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
+// --^-- write your code here --^--
