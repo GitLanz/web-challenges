f2abd00d03559dc6fe5fd2000f1da6466710f25f
Brigitte Lanz <brigitte.lanz.neuefische@gmail.com>
2025-02-28 12:47:36 +0100

    add function inside function

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello world!</h1>
+      <p>Text</p>
+    </article>
+  );
 }
