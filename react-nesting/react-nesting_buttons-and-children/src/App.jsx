7f552f3f3816c120afdbeff2aa8ff80aafd5665d
Brigitte Lanz <brigitte.lanz.neuefische@gmail.com>
2025-03-03 11:01:29 +0100

    use children in button component

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click me one time!</Button>
+      <Button>Click me two times!</Button>
+      <Button>Click me three times!</Button>
+      <Button>Click me four times!</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
