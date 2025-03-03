a80cbc24e75a7c21ee1c396a98e52969a7f2b6a7
Brigitte Lanz <brigitte.lanz.neuefische@gmail.com>
2025-03-03 16:53:51 +0100

    refractor the code

--- react-state-1/react-state-1_counter/src/App.jsx
+++ react-state-1/react-state-1_counter/src/App.jsx
@@ -1,26 +1,31 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let count = 0;
+  // let count = 0;
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+function Counter() {
+  function handleClick1() {
+    setCount(count + 1);
+  }
+  function handleClick2() {
+    setCount(count - 1);
+  }
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleClick2}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleClick1}>
           +
         </button>
       </div>
