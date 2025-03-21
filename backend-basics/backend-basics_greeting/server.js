1a7b95c56cf7e90443c1eaab91d0c96f553d2400
Brigitte Lanz <brigitte.lanz.neuefische@gmail.com>
2025-03-21 11:43:43 +0100

    add console.log

--- backend-basics/backend-basics_greeting/server.js
+++ backend-basics/backend-basics_greeting/server.js
@@ -1,6 +1,7 @@
 import { createServer } from "node:http";
 
 export const server = createServer((req, res) => {
+  console.log(req.url);
   res.statusCode = 200;
   res.end("Hello, [your name]!");
 });
